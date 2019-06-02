871f788bb2f50e4730ea2ad9ebec8464d6865e88
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-06-02 13:25:29 -0400

    Criando CategoriaController

---
+++ Desenvolvimento Web 2 - Rodrigo/Controllers/CategoriaController.cs
@@ -0,0 +1,37 @@
+﻿using Desenvolvimento_Web_2___Rodrigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Desenvolvimento_Web_2___Rodrigo.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        DevWeb2Entities ctx = new DevWeb2Entities();
+
+        // GET: Categoria
+        public ActionResult Index()
+        {            
+            return View(ctx.CATEGORIA.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CATEGORIA c)
+        {
+            if (ModelState.IsValid) {
+                ctx.CATEGORIA.Add(c);
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(c);
+        }
+    }
+}
