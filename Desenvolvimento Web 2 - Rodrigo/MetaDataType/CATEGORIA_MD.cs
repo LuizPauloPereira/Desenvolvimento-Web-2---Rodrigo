e5cf0e029a74a2f15c9d3fc9914e14c13c55d415
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-06-02 12:59:14 -0400

    Criando metadados para models

---
+++ Desenvolvimento Web 2 - Rodrigo/MetaDataType/CATEGORIA_MD.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Desenvolvimento_Web_2___Rodrigo.MetaDataType
+{
+    [MetadataType(typeof(CATEGORIA_MD))]
+    public partial class CATEGORIA { }
+
+
+    public partial class CATEGORIA_MD
+    {
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "campo obrigatório")]
+        [StringLength(50, ErrorMessage = "Máximo de 50 Caractéres")]
+        public string Descricao { get; set; }
+    }
+}
---
+++ Desenvolvimento Web 2 - Rodrigo/MetaDataType/PRODUTO_MD.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Desenvolvimento_Web_2___Rodrigo.MetaDataType
+{
+    [MetadataType(typeof(PRODUTO_MD))]
+    public partial class PRODUTO { }
+
+
+    public partial class PRODUTO_MD
+    {
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "campo obrigatório")]
+        [StringLength(50, ErrorMessage ="Máximo de 50 Caractéres")]
+        public string Descricao { get; set; }
+
+        [DisplayName("Preço")]
+        [Required(ErrorMessage = "campo obrigatório")]
+        [MaxLength(18, ErrorMessage ="Máximo 18 caractéres")]
+        public decimal Valor { get; set; }
+
+
+        [DisplayName("Id da categoria")]
+        [Required(ErrorMessage = "campo obrigatório")]
+        public int CategoriaId { get; set; }
+    }
+}
